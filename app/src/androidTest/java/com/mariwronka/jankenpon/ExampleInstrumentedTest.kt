8a425b17bc9d71fc180bec649312b1f39d7e46ce
Mari Wronka 👩🏻‍🦰 <marielle.wronka@gmail.com>
2025-07-26 18:52:52 -0300

    Update ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/mariwronka/jankenpon/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/mariwronka/jankenpon/ExampleInstrumentedTest.kt
@@ -10,7 +10,6 @@ import org.junit.runner.RunWith
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
-        // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.mariwronka.jankenpon", appContext.packageName)
     }
